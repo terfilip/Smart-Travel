c13b02049be289e96435ac6c0e4c3359d873b935
MaciejM <maciejm@WORKGROUP>
2014-10-25 04:25:35 +0100

    Hi there :)

--- php/core.php
+++ php/core.php
@@ -1,6 +1,33 @@
 <?php
+$q = $_POST['q'];
+$host = "192.168.1.3";
+$username = "root";
+$password = "password";
+$db = "hackathon";
+$con = mysqli_connect($host,$username,$password,$db);
+if (!$con)
+  {
+  die('Could not connect: ' . mysqli_error($con));
+}
 
-echo "Hello World";
+mysqli_select_db($con,$db);
 
+if($q == "getCountries") {
+	$sql = "Select Country from Countries";
+	$result = mysqli_query($con,$sql);
+	
+	while($row = mysqli_fetch_array($result))
+	{
+		echo '<option>'.$row['Country'].'</option>';
+	}
+}
 
+if($q == "getRegions") {
+	$sql = "Select Region from Regions";
+	$result = mysqli_query($con,$sql);
+	while($row = mysqli_fetch_array($result))
+	{
+		echo '<option>'.$row['Region'].'</option>';
+	}
+}
 ?>
