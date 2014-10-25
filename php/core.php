1e43540a561b9be9424cde1d015a7f9f063d59d7
MaciejM <maciejm@WORKGROUP>
2014-10-25 03:33:59 +0100

    Shhh... it's some PHP

---
+++ php/core.php
@@ -0,0 +1,32 @@
+<?php
+$q = $_['GET'];
+$host = "192.168.1.3";
+$username = "root";
+$password = "password";
+$db = "hackathon";
+$con = mysqli_connect($host,$username,$password,$db);
+if (!$con)
+  {
+  die('Could not connect: ' . mysqli_error($con));
+}
+
+mysqli_select_db($con,$db);
+
+if($q == "getCountries") {
+	$sql = "Select Country from Countries";
+	mysqli_query($con,$sql);
+	while($row = mysqli_fetch_array($result))
+	{
+		echo '<option>'.$row['Country'].'</option>';
+	}
+}
+
+if($q == "getCountries") {
+	$sql = "Select Region from Regions";
+	mysqli_query($con,$sql);
+	while($row = mysqli_fetch_array($result))
+	{
+		echo '<option>'.$row['Region'].'</option>';
+	}
+}
+?>
