f8e6d3372289630c0ec20bfc3db1466dc850f079
Filip-Ter <filip.ter@gmail.com>
2014-10-25 12:13:06 +0100

    changin number format

--- php/core.php
+++ php/core.php
@@ -15,7 +15,7 @@ mysqli_select_db($con,$db);
 if($q == "getCountries") {
 	$sql = "Select Country from Countries";
 	$result = mysqli_query($con,$sql);
-	
+
 	while($row = mysqli_fetch_array($result))
 	{
         $cell = $row['Country'];
@@ -35,22 +35,22 @@ if($q == "getRegions") {
 	}
 }
 if($q == "getRankings") {
-	//Getting Time	
+	//Getting Time
 	$time = $_POST['time'];
-	
+
 	//Getting current Country's Data and retrieving Ranking. More can be retrieved.
 	$curLocSQL = "SELECT  *, (CPI+RI+CPPRI+GI+RPI+LPP)/6 as `Ranking` from Averages  where Country = '".$_POST['country']."' order by `Ranking` ASC";
 	$result = mysqli_query($con,$curLocSQL);
 	$currCountryData = mysqli_fetch_array($result);
 	$cur_Ranking = $currCountryData['Ranking'];
-	
+
 	//Retrieving Current Price. If the Price is Not available, N/A will be printed.
 	$cur_PriceSQL = "Select Price from foodPrice where Country = '".$_POST['country']."'";
 	$result = mysqli_query($con,$cur_PriceSQL);
 	$row = mysqli_fetch_array($result);
 	$cur_Price = $row['Price'];
 	if($cur_Price == "") $cur_Price = "N/A";
-	
+
 	//Retrieving Country Cards and comparing Values as compared to current country data.
 	$region = $_POST['region'];
 	$sql = "SELECT  *, (CPI+RI+CPPRI+GI+RPI+LPP)/6 as `Ranking`,Price from Averages INNER JOIN foodPrice on Averages.Country = foodPrice.Country where Region = '".$region."' order by `Ranking` ASC";
@@ -63,11 +63,11 @@ if($q == "getRankings") {
 			echo '<table>';
 			echo '<tr>';
 				echo '<td class="what">Average Daily Spending</td>';
-				echo '<td>$'.$row['Price'].'</td>';
+				echo '<td>$'.number_format($row['Price'],2).'</td>';
 			echo '</tr>';
 			echo '<tr>';
 				echo '<td class="what">Predicted Food Holiday Spending</td>';
-				echo '<td>$'.($row['Price']*$time[0]*7).'</td>';
+				echo '<td>$'.number_format(($row['Price']*$time[0]*7),2).'</td>';
 			echo '</tr>';
 			echo '</table>';
 			echo '</div>';
@@ -83,11 +83,11 @@ if($q == "getRankings") {
 			echo '<table>';
 			echo '<tr>';
 				echo '<td class="what">Average Daily Spending</td>';
-				echo '<td>$'.$row['Price'].'(You\'re Saving: '.($cur_Price-$row['Price']).')</td>';
+				echo '<td>$'.number_format($row['Price'],2).'(You\'re Saving: $'.number_format(($cur_Price-$row['Price']),2).')</td>';
 			echo '</tr>';
 			echo '<tr>';
 				echo '<td class="what">Predicted Food Holiday Spending</td>';
-				echo '<td>$'.($row['Price']*$time[0]*7).'</td>';
+				echo '<td>$'.number_format(($row['Price']*$time[0]*7),2).'</td>';
 			echo '</tr>';
 			echo '</table>';
 			echo '</div>';
